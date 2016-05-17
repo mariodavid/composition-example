eb445e85e2d808bd35c6a33fd0539be2b69c364c
Mario David <mariodavid@arcor.de>
2016-05-17 12:02:48 +0200

    removed current entity from composite lookup as well as all subelements

--- modules/web/src/com/company/compositionexample/web/composite/CompositeBrowse.java
+++ modules/web/src/com/company/compositionexample/web/composite/CompositeBrowse.java
@@ -9,6 +9,7 @@ import com.haulmont.cuba.gui.components.TreeTable;
 import com.haulmont.cuba.gui.data.HierarchicalDatasource;
 
 import javax.inject.Inject;
+import java.util.Collection;
 import java.util.Map;
 import java.util.UUID;
 
@@ -29,9 +30,26 @@ public class CompositeBrowse extends AbstractLookup {
         compositesTable.refresh();
 
         if (params.containsKey("exclude")) {
-            compositesDs.excludeItem((Composite) params.get("exclude"));
+            Composite currenctComponent = (Composite) params.get("exclude");
+            removeElementAndSubelementsFromTree(currenctComponent.getId());
         }
+    }
 
+    private void removeSubelementsFromTree(UUID parent) {
+        Collection<UUID> children = compositesDs.getChildren(parent);
+
+        for(UUID child : children) {
+            if (compositesDs.hasChildren(child)) {
+                removeSubelementsFromTree(child);
+            }
+            Composite currentItem = compositesDs.getItem(child);
+            compositesDs.excludeItem(currentItem);
+        }
+    }
+
+    private void removeElementAndSubelementsFromTree(UUID parent) {
+        removeSubelementsFromTree(parent);
+        compositesDs.excludeItem(compositesDs.getItem(parent));
     }
 
 }
